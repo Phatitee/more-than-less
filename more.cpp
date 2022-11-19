e26d19d4c75f3679fc6205116aa080e4869d319e
Phizpat <117262852+Phatitee@users.noreply.github.com>
2022-11-19 22:08:22 +0700

    Add files via upload

---
+++ more.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+	int main(){
+		int num1,num2;
+		printf("Number1 : ");
+		scanf("%d", &num1);
+		printf("Number2 : ");
+		scanf("%d", &num2);
+		if (num1 > num2){
+		printf("%d",num1);
+		}
+		else if (num1 < num2){
+		printf("%d",num2);	
+		}
+		else{
+			printf("Equal");
+		}
+	
+		
+		
+		
+		return 0;
+	}
+		
